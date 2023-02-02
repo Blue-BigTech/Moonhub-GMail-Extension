4fd26d8b7181fd7849060a23657db8113fadb3b6
BlueBigTech <blue.bigtech@gmail.com>
2023-02-02 16:07:48 -0500

    add fetch

--- src/content.js
+++ src/content.js
@@ -6,10 +6,39 @@ InboxSDK.load(2, "sdk_moonhub-inbox_d80d2bf259").then((sdk) => {
     // a compose view has come into existence, do something with it!
     composeView.addButton({
       title: "Moonhub",
-      iconUrl:
-        "https://lh5.googleusercontent.com/itq66nh65lfCick8cJ-OPuqZ8OUDTIxjCc25dkc4WUT1JG8XG3z6-eboCu63_uDXSqMnLRdlvQ=s128-h128-e365",
+      iconUrl: chrome.runtime.getURL('images/icon.png'),
       onClick(event) {
-        event.composeView.insertTextIntoBodyAtCursor("Hello World!");
+        let threadID = composeView.getThreadID();
+        if(threadID == ''){
+          console.log('This is new email. Not reply!');
+        }else{
+          fetch(`https://email-generation-backend-dev-ggwnhuypbq-uc.a.run.app/ai-emails/${threadID}`,{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+          })
+          .then(res => res.json())
+          .then(json => {
+            console.log(json.ai_emails)
+          });  
+          // axios.get(`https://api.chucknorris.io/jokes/random`).then(res => {console.log(res.data);});
+          // axios.get(`https://email-generation-backend-dev-ggwnhuypbq-uc.a.run.app/thread-emails/${threadID}`, {
+          //     headers : {
+          //     'Content-Type' : 'application/json'
+          //   }
+          // })
+          // .then(res => {
+          //   if (res.status === 200) {
+          //     email_contents = res.data.emails[0];
+          //     console.log(email_contents);
+          //     email_contents = '<pre style="white-space : pre-wrap">' + email_contents + '</pre>';
+          //     composeView.setBodyHTML(email_contents);   
+          //   } else {
+          //     console.log(res.error);
+          //   }
+          // });
+        }
       },
     });
   });
